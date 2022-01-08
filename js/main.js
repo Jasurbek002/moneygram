7ce60ae8426aefe294b0df7e5288ad3e55846568
unknown <jasurbekturgunov25@gmail.com>
2022-01-08 13:03:36 +0500

    stillandi

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 var form = document.querySelector('#form')
 var input = document.querySelector('#mathc')
+var xato = document.getElementById('xato')
 var turi = document.querySelector('#turi')
 var hsoblash = document.querySelector('#resalt')
 
 form.addEventListener('submit', function(event){
     event.preventDefault()
 
+    var a = input.value
+    var b = turi.value
+    
 
-    hsoblash.textContent = input.value/turi.value
+    if(a< 100000){
+        xato.textContent='100000 so\'mdan past qimat hisoblanmaydi!!!'
+        xato.style.color='red'
+        
+    }else{
+        hsoblash.textContent = a/b
+        hsoblash.style.color='green'
+        xato.style.color='white'
+
+       
+        
+    
+       
+    }
+    
+         
+
+  
 
 })
